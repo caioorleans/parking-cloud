526d980fb9fce3fc38018bf3e1f258760a7f12ba
caioorleans <caioorleansdm@hotmail.com>
2022-08-16 20:51:38 -0300

    Criado o findById e create

--- src/main/java/cloudparking/controller/ParkingController.java
+++ src/main/java/cloudparking/controller/ParkingController.java
@@ -2,12 +2,17 @@ package cloudparking.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cloudparking.controller.mapper.ParkingMapper;
+import cloudparking.dto.ParkingCreateDTO;
 import cloudparking.dto.ParkingDTO;
 import cloudparking.model.Parking;
 import cloudparking.service.ParkingService;
@@ -29,6 +34,21 @@ public class ParkingController {
 		List<Parking> parkingList = service.findAll();
 		List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
 		return ResponseEntity.ok(result);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ParkingDTO> findById(@PathVariable String id){
+		Parking parking = service.findById(id);
+		ParkingDTO parkingDTO = parkingMapper.toParkingDTO(parking);
+		return ResponseEntity.ok(parkingDTO);
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO parkingDTO){
+		Parking parkingCreate = parkingMapper.toParking(parkingDTO);
+		Parking parking = service.create(parkingCreate);
+		ParkingDTO result = parkingMapper.toParkingDTO(parking);
+		return ResponseEntity.status(HttpStatus.CREATED).body(result);
 	}
 
 }
--- src/main/java/cloudparking/service/ParkingService.java
+++ src/main/java/cloudparking/service/ParkingService.java
@@ -1,6 +1,6 @@
 package cloudparking.service;
 
-import java.util.Arrays;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,4 +29,18 @@ public class ParkingService {
 	public List<Parking> findAll(){
 		return parkingMap.values().stream().collect(Collectors.toList());
 	}
+
+	public Parking findById(String id) {
+		return parkingMap.get(id);
+	}
+
+	public Parking create(Parking parkingCreate) {
+		String uuid = getUUID();
+		parkingCreate.setId(uuid);
+		parkingCreate.setEntryDate(LocalDateTime.now());
+		parkingMap.put(uuid,parkingCreate);
+		return parkingCreate;
+	}
+	
+	
 }
