5c6174554a34fd1ab7cc112e70387034d0b1edfd
caioorleans <caioorleansdm@hotmail.com>
2022-08-17 19:12:46 -0300

    Criados testes do findAll e create

--- src/test/java/cloudparking/ParkingControllerIT.java
+++ src/test/java/cloudparking/ParkingControllerIT.java
@@ -1,10 +1,14 @@
 package cloudparking;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import cloudparking.dto.ParkingCreateDTO;
 import io.restassured.RestAssured;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,8 +23,35 @@ public class ParkingControllerIT {
 	}
 	
 	@Test
-	void findAll() {}
+	void whenFindAllThenCheckResult() {
+		RestAssured.given()
+			.when()
+			.get("/parking")
+			.then()
+			.statusCode(HttpStatus.OK.value())
+			.body("license[0]", Matchers.equalTo("DMS-1111"))
+			.extract().response().body().prettyPrint();
+	}
 	
 	@Test
-	void create() {}
+	void whenCreateThenCheckIsCreated() {
+		
+		ParkingCreateDTO dto = new ParkingCreateDTO();
+		dto.setColor("vermelho");
+		dto.setLicense("URT-5555");
+		dto.setModel("celta");
+		dto.setState("CE");
+		
+		RestAssured.given()
+		.when()
+		.contentType(MediaType.APPLICATION_JSON_VALUE)
+		.body(dto)
+		.post("/parking")
+		.then()
+		.statusCode(HttpStatus.CREATED.value())
+		.body("license", Matchers.equalTo("URT-5555"))
+		.body("color", Matchers.equalTo("vermelho"))
+		.body("model", Matchers.equalTo("celta"))
+		.body("state", Matchers.equalTo("CE"));
+	}
 }
