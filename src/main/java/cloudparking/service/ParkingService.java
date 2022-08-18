f9d63a40ab2856375b3ba5d1f91957be82491500
caioorleans <caioorleansdm@hotmail.com>
2022-08-18 12:47:55 -0300

    Adicionando anotação Transactional

--- src/main/java/cloudparking/service/ParkingService.java
+++ src/main/java/cloudparking/service/ParkingService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import cloudparking.exception.ParkingNotFoundException;
 import cloudparking.model.Parking;
@@ -23,15 +25,18 @@ public class ParkingService {
 		return UUID.randomUUID().toString().replace("-", "");
 	}
 	
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public List<Parking> findAll(){
 		return parkingRepository.findAll();
 	}
 
+	@Transactional(readOnly = true)
 	public Parking findById(String id) {
 		return parkingRepository.findById(id).orElseThrow(() -> new ParkingNotFoundException(id));
 		
 	}
-
+	
+	@Transactional
 	public Parking create(Parking parkingCreate) {
 		String uuid = getUUID();
 		parkingCreate.setId(uuid);
@@ -40,11 +45,13 @@ public class ParkingService {
 		return parkingCreate;
 	}
 
+	@Transactional
 	public void delete(String id) {
 		Parking parking = findById(id);
 		parkingRepository.deleteById(id);
 	}
 
+	@Transactional
 	public Parking update(String id, Parking parkingCreate) {
 		Parking parking = findById(id);
 		parking.setColor(parkingCreate.getColor());
@@ -55,6 +62,7 @@ public class ParkingService {
 		return parking;
 	}
 
+	@Transactional
 	public Parking checkout(String id) {
 		Parking parking = findById(id);
 		parking.setExitDate(LocalDateTime.now());
---
+++ src/test/java/cloudparking/ParkingCheckOutTest.java
@@ -0,0 +1,11 @@
+package cloudparking;
+
+import org.junit.jupiter.api.Test;
+
+public class ParkingCheckOutTest {
+	
+	@Test
+	void getBill() {
+		
+	}
+}
